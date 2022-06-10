baa064b5975ea6a6728f8efd25a28895c1137ae9
Agemasie Amare <agemasie@gmail.com>
2022-06-10 10:51:51 +0000

    Task 5 Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* print_sign - Returns 1 and prints + if n is >0, Returns 0 n=0, Returns -1 n <0
+* @c: takes in a character
+* Return: 0 for uppercase, 1 for lowercase
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+		else if (n == 0)
+		{
+			_putchar(48);
+			return (0);
+		}
+	      else
+	{
+		_putchar(45);
+		return (-1);
+	}
+}
