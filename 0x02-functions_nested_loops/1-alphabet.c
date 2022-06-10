4136f2d1dcd6c18b243452a03e47055518775ce3
Agemasie Amare <agemasie@gmail.com>
2022-06-10 11:56:34 +0000

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,15 +2,16 @@
 
 /**
 * print_alphabet -> prints the lowercase alphabets
+* Return: Always 0.
 */
+void print_alphabet(void)
 
-	void print_alphabet(void)
 {
 	int j;
 
 	for (j = 'a'; j <= 'z'; j++)
 	{
-		 _putchar(j);
+		_putchar(j);
 	}
 	_putchar('\n');
 }
