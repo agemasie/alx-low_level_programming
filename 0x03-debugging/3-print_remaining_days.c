e81bda0103ecbdfb46504613e00859ca652f037d
Agemasie Amare <agemasie@gmail.com>
2022-06-14 13:47:24 +0000

    Task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -13,9 +13,9 @@
 void print_remaining_days(int month, int day, int year)
 
 {
-	if (year % 4 == 0 || (year % 400 == 0 && year % 100 == 0))
+	if ((year % 4 == 0 || (year % 400 == 0) && ! (year % 100 == 0))
 	{
-		if (month > 2 && day >= 60)
+		if (month >= 2 && day >= 60)
 		{
 			day++;
 		}
@@ -36,4 +36,4 @@ void print_remaining_days(int month, int day, int year)
 			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
-}
+} 
