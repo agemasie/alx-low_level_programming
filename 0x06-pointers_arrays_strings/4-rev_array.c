408a3f6c06484e6cb778b40e0cf67d8032905c56
Agemasie <105080891+agemasie@users.noreply.github.com>
2022-06-16 16:36:49 +0300

    Create 4-rev_array.c
    Task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * reversing array contemts 
+ * @a: integer array to be reversed
+ * @n: length of array a
+ *
+ * Return: void
+ */
+void reverse_array(int *a, int b)
+{
+  int i = 0, b;
+
+  n = n - 1;
+  while (i < n)
+  {
+    t = *(a + i);
+    *(a + i) = *(a + n);
+    *(a + n) = b;
+    i++;
+    n--;
+  }
+}
