32e2269fbcc9aed4919cae03203ffedc311ba69f
Agemasie <105080891+agemasie@users.noreply.github.com>
2022-07-09 08:00:56 +0300

    Update 1-memcpy.c

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
 
 /**
-* _memcpy - irontnoux2
-* @dest: pointer to char params
-* @src: pointer to char params
-* @n: size
-* Return: *dest
-*/
+ * _memcpy - irontnoux2
+ * @dest: pointer to char params
+ * @src: pointer to char params
+ * @n: size
+ * Return: *dest
+ */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
