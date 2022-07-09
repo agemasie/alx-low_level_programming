d29e68c740cbeb53b97f8318441edc453de514b5
Agemasie <105080891+agemasie@users.noreply.github.com>
2022-07-09 08:05:37 +0300

    Update 2-strlen.c

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 
 /**
-* _strlen - length of a string
-* @s: input char
-* Return: length of a string
-*/
+ * _strlen - length of a string
+ * @s: input char
+ * Return: length of a string
+ */
 
 int _strlen(char *s)
 {
