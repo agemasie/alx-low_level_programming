6f719dd718c1fae2c628a29150e42f3b9fce41fc
Agemasie <105080891+agemasie@users.noreply.github.com>
2022-07-09 09:53:35 +0300

    Update 2-print_strings.c
    Task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,29 +1,38 @@
 #include <stdio.h>
+#include <stdarg.h>
+
 /**
- * print_strings - prints strings, followed by a new line
- * @separator: string to be printed between strings
- * @n: number of strings passed to function
- * Return: nothing
+ * print_strings - prints numbers
+ * @separator: pointer to the string to be printed between strings
+ * @n: number of strings passed to the function
  */
+
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	va_list ap;
 	unsigned int i;
-	char *og;
+	va_list params;
+	char *str;
 
-	va_start(ap, n);
+	va_start(params, n);
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < (n - 1) && n != 0; i++)
 	{
-		og = (va_arg(ap, char*));
-		if (og != NULL)
-			printf("%s", og);
+		str = va_arg(params, char *);
+		if (!separator)
+			printf("%s", str ? str : "(nil)");
 		else
-			printf("(nil)");
-
-		if (separator != NULL && i < n - 1)
-			printf("%s", separator);
+			printf("%s%s", str ? str : "(nil)", separator);
 	}
-	printf("\n");
-	va_end(ap);
+
+	if (n)
+	{
+		str = va_arg(params, char *);
+		printf("%s\n", str ? str : "(nil)");
+	}
+	else
+	{
+		printf("\n");
+	}
+
+	va_end(params);
 }
